f8868340ee9bb7be46e394095a928497020c83c5
Camik13 <camikchristensen@gmail.com>
2022-03-30 15:03:45 -0600

    Add project files.

---
+++ Components/TeamsViewComponent.cs
@@ -0,0 +1,32 @@
+﻿using BowlingTeam.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BowlingTeam.Components
+{
+    public class TeamsViewComponent : ViewComponent
+    {
+        private BowlingDbContext _context { get; set; }
+
+        public TeamsViewComponent (BowlingDbContext temp)
+        {
+            _context = temp;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.SelectedType = RouteData?.Values["team"];
+
+            var teams = _context.Bowlers
+                .Select(x => x.Team.TeamName) // WAS: x => x.Team.TeamName
+                .Distinct()
+                .OrderBy(x => x);
+
+            return View(teams);
+        }
+       
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,109 @@
+﻿using BowlingTeam.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BowlingTeam.Controllers
+{
+    public class HomeController : Controller
+    {
+        private BowlingDbContext _context { get; set; }
+
+        //Constructor
+        public HomeController(BowlingDbContext temp)
+        {
+            _context = temp;
+        }
+
+        public IActionResult Index(string team)
+        {
+
+            ViewBag.Teams = _context.Teams.ToList();
+
+            if (team == null)
+            {
+                team = "All";
+            }
+
+            ViewBag.TeamName = "- " + team;
+
+            var data = _context.Bowlers
+                .Where(t => t.Team.TeamName == team || team == "All")
+                .Include(x => x.Team)
+                .OrderBy(n => n.BowlerLastName)
+                .ToList();
+
+            return View(data);
+        }
+
+        [HttpGet]
+        public IActionResult AddBowler()
+        {
+            ViewBag.Teams = _context.Teams.ToList();
+
+            Bowler b = new Bowler();
+
+            return View("AddBowler", b);
+        }
+
+        [HttpPost]
+        public IActionResult AddBowler(Bowler bowler)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(bowler);
+                _context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ViewBag.Teams = _context.Teams.ToList();
+
+                return View(bowler);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int bowlerID)
+        {
+            ViewBag.Teams = _context.Teams.ToList();
+            var bowler = _context.Bowlers.Single(x => x.BowlerID == bowlerID);
+
+            return View("AddBowler", bowler);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Bowler bowler)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Update(bowler);
+                _context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ViewBag.Teams = _context.Teams.ToList();
+
+                return View("AddBowler", bowler);
+            }
+
+           
+        }
+
+        public IActionResult Delete (Bowler bowler)
+        {
+            _context.Bowlers.Remove(bowler);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
